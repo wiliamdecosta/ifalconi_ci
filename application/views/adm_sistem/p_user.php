5d0a062c81e59b084097f295b6456bc665831990
wiliamdecosta <wiliamdecosta@gmail.com>
2015-07-09 15:08:35 +0700

    ''

---
+++ application/views/adm_sistem/p_user.php
@@ -0,0 +1,147 @@
+<!-- Bootgrid Dialog -->
+<link rel="stylesheet" href="<?php echo BS_PATH; ?>bootgrid/jquery.bootgrid.css" />
+<link rel="stylesheet" href="<?php echo BS_PATH; ?>bootgrid/modification.css" />
+<script src="<?php echo BS_PATH; ?>bootgrid/jquery.bootgrid.min.js"></script>
+<script src="<?php echo BS_PATH; ?>bootgrid/properties.js"></script>
+
+<div class="page-header">
+	<h1>
+		Parameter
+		<small>
+			<i class="ace-icon fa fa-angle-double-right"></i>
+			User
+		</small>
+	</h1>
+</div><!-- /.page-header -->
+
+<div class="row" id="user_row_content" style="display:none;">
+	<div class="col-xs-12">
+		<!-- PAGE CONTENT BEGINS -->
+		<div class="row">
+		    <div class="col-xs-12">
+		        <div class="well well-sm">
+		            <div class="inline middle pink2 bigger-150"> User List </div>
+		        </div>
+		        <p>
+					<button class="btn btn-white btn-success btn-round" id="user_btn_add">
+						<i class="ace-icon glyphicon glyphicon-plus bigger-120 green"></i>
+					    Add
+					</button>
+
+					<button class="btn btn-white btn-danger btn-round" id="user_btn_delete">
+						<i class="ace-icon glyphicon glyphicon-trash bigger-120 red"></i>
+						Delete
+					</button>
+				</p>
+
+		        <table id="user_grid_selection" class="table table-striped table-bordered table-hover">
+                <thead>
+                  <tr>
+                    <th data-identifier="true" data-visible="false" data-header-align="center" data-align="center" data-column-id="p_user_id"> ID User</th>
+                     <th data-header-align="center" data-align="center" data-formatter="opt-edit" data-sortable="false" data-width="100">Options</th>
+                     <th data-column-id="user_name" data-header-align="center" data-align="center" data-width="150">User Name</th>
+                     <th data-column-id="full_name"> Full Name </th>
+                     <th data-column-id="user_status"> Status </th>
+                     <th data-column-id="email_address"> Email </th>
+                  </tr>
+                </thead>
+              </table>
+		    </div>
+	    </div>
+        <!-- PAGE CONTENT ENDS -->
+	</div><!-- /.col -->
+</div><!-- /.row -->
+
+<?php $this->load->view('adm_sistem/p_user_add_edit.php'); ?>
+
+<script>
+    jQuery(function($) {
+        user_prepare_table();
+
+        /* show content */
+        $("#user_grid_selection").bootgrid().on("loaded.rs.jquery.bootgrid", function (e){
+           $("#user_row_content").slideDown("fast", function(){});
+        });
+
+        $("#user_btn_add").on(ace.click_event, function() {
+            user_show_form_add();
+        });
+
+        $("#user_btn_delete").on(ace.click_event, function(){
+            if($("#user_grid_selection").bootgrid("getSelectedRows") == "") {
+                showBootDialog(true, BootstrapDialog.TYPE_INFO, 'Information', 'Plese select <span class="glyphicon glyphicon-check" /> data on the table to execute delete operation');
+            }else {
+                user_delete_records( $("#user_grid_selection").bootgrid("getSelectedRows") );
+            }
+        });
+
+    });
+
+    function user_prepare_table() {
+        $("#user_grid_selection").bootgrid({
+    	     formatters: {
+                "opt-edit" : function(col, row) {
+                    return '<a href="#" onclick="user_show_form_edit(\''+ row.p_user_id +'\')" class="green"><i class="ace-icon fa fa-pencil bigger-130"></i></a> &nbsp; <a href="#" onclick="user_delete_records(\''+ row.p_user_id +'\')" class="red"><i class="ace-icon glyphicon glyphicon-trash bigger-130"></i></a>';
+                }
+             },
+    	     rowCount:[10,25,50,100,-1],
+    		 ajax: true,
+    	     requestHandler:function(request) {
+    	        if(request.sort) {
+    	            var sortby = Object.keys(request.sort)[0];
+    	            request.dir = request.sort[sortby];
+
+    	            delete request.sort;
+    	            request.sort = sortby;
+    	        }
+    	        return request;
+    	     },
+    	     responseHandler:function (response) {
+    	        if(response.success == false) {
+    	            showBootDialog(true, BootstrapDialog.TYPE_DANGER, 'Warning', response.message);
+    	        }
+    	        return response;
+    	     },
+       	     url: '<?php echo WS_URL2."adm_sistem.p_user_controller/read"; ?>',
+    	     selection: true,
+    	     multiSelect: true,
+    	     sorting:true,
+    	     rowSelect:true,
+    	     labels: {
+    	        loading     : properties.bootgridinfo.loading
+	         }
+    	});
+    	resize_bootgrid();
+    }
+
+    function user_reload_table() {
+        $("#user_grid_selection").bootgrid("reload");
+    }
+
+    function user_delete_records(theID) {
+        BootstrapDialog.confirm({
+            type: BootstrapDialog.TYPE_WARNING,
+		    title:'Delete Confirmation',
+		    message: 'Do you really want to delete the data(s)?',
+		    btnCancelLabel: 'Cancel',
+            btnOKLabel: 'Yes, Delete',
+		    callback: function(result) {
+    	        if(result) {
+    	            $.post( "<?php echo WS_URL.'adm_sistem.p_user_controller/destroy'; ?>",
+            		    { items: JSON.stringify(theID) },
+                        function( response ) {
+                            if(response.success == false) {
+                	            showBootDialog(true, BootstrapDialog.TYPE_DANGER, 'Warning', response.message);
+                	        }else {
+                    	        loadContent('adm_sistem-p_user');
+                                showBootDialog(true, BootstrapDialog.TYPE_SUCCESS, 'Information', response.message);
+                            }
+                        }
+                	);
+    	        }
+		    }
+		});
+
+    }
+
+</script>
---
+++ application/views/adm_sistem/p_user_add_edit.php
@@ -0,0 +1,137 @@
+<div class="row" style="display:none;" id="user_form_add_edit">
+    <div class="col-xs-12">
+        <div class="well well-sm">
+		    <div class="inline middle blue bigger-150" id="user_form_title"> Add/Edit Bank </div>
+		</div>
+        <form class="form-horizontal" role="form" id="user_form">
+            <div class="form-group">
+                <label class="col-sm-3 control-label no-padding-right"> User Name *</label>
+                <div class="col-sm-9">
+                    <input id="form_p_user_id" type="hidden" placeholder="Area ID">
+                    <input id="form_user_name" class="col-xs-10 col-sm-5 required" type="text">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-sm-3 control-label no-padding-right"> Description </label>
+                <div class="col-sm-9">
+                    <textarea id="form_description" class="col-xs-10 col-sm-5" type="text"></textarea>
+                </div>
+            </div>
+            
+            <?php
+			    $ci =& get_instance();
+	            $user_name = $ci->session->userdata('user_name');
+			?>
+            <div class="form-group">
+                <label class="col-sm-3 control-label no-padding-right"> Created By </label>
+                <div class="col-sm-9">
+                    <input id="form_created_by" disabled type="text" value="<?php echo $user_name; ?>">
+                    &nbsp;  <input id="form_creation_date" disabled type="text" value="<?php echo date("Y-m-d"); ?>">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-sm-3 control-label no-padding-right"> Updated By </label>
+                <div class="col-sm-9">
+                    <input id="form_updated_by" disabled type="text" value="<?php echo $user_name; ?>">
+                    &nbsp; <input id="form_updated_date" disabled type="text" value="<?php echo date("Y-m-d"); ?>">
+                </div>
+            </div>
+
+           <div class="space-4"></div>
+
+           <div class="clearfix form-actions">
+		        <div class="col-md-offset-3 col-md-9">
+			      	<button type="button" class="btn btn-primary btn-round" id="user_form_btn_save">
+			      		<i class="ace-icon fa fa-floppy-o bigger-120"></i>
+			      		Save
+			      	</button>
+                    
+                    <button type="reset" class="btn btn-danger btn-round" id="user_form_btn_cancel">
+                        <i class="glyphicon glyphicon-circle-arrow-left bigger-120"></i>
+                        Cancel
+                    </button>
+			      	
+			    </div>
+		   </div>
+       </form>
+    </div>
+</div>
+
+
+<script>
+    jQuery(function($) {
+        $("#user_form_btn_cancel").on(ace.click_event, function() {
+            user_toggle_main_content();
+        });
+    
+        $("#user_form_btn_save").on(ace.click_event, function() {
+            user_save();
+        });
+    });
+
+    function user_toggle_main_content() {
+        $("#user_form")[0].reset();
+        $("#user_form_add_edit").hide();
+        $("#user_row_content").toggle("slow");
+    }
+
+    function user_show_form_add() {
+        user_toggle_main_content();
+        $("#user_form_add_edit").show("slow");
+        $("#user_form_title").html("Add User");
+    }
+
+    function user_show_form_edit(theID) {
+        user_toggle_main_content();
+        $("#user_form_add_edit").show("slow");
+        $("#user_form_title").html("Edit User");
+        
+        $("#form_p_user_id").val(theID);
+        $.post( "<?php echo WS_URL.'adm_sistem.p_user_controller/read'; ?>",
+            {
+                p_user_id : $("#form_p_user_id").val()
+            },
+            function( response ) {
+                if(response.success == false) {
+                    showBootDialog(true, BootstrapDialog.TYPE_DANGER, 'Warning', response.message);
+                }else {
+        	        var obj = response.items[0];
+        	        
+        	        $("#form_p_user_id").val(obj.p_user_id);
+        	        $("#form_description").val(obj.description);
+        	        
+        	        $("#form_created_by").val(obj.created_by);
+        	        $("#form_creation_date").val(obj.creation_date);
+        	        $("#form_updated_by").val(obj.updated_by);
+        	        $("#form_updated_date").val(obj.updated_date);
+        	        
+                }
+            }
+        );
+        
+    }
+
+    function user_save() {
+        var action_execute = "";
+
+        //jika ID kosong, panggil method create. Jika ID ada, maka panggil method update
+        action_execute = ( $("#form_p_user_id").val() == "") ? "create" : "update";
+        $.post( "<?php echo WS_URL.'adm_sistem.p_user_controller/'; ?>" + action_execute,
+            {items: JSON.stringify({
+                    p_user_id           : $("#form_p_user_id").val(),
+                    description         : $("#form_description").val()
+                })
+            },
+            function( response ) {
+                if(response.success == false) {
+                    showBootDialog(true, BootstrapDialog.TYPE_DANGER, 'Warning', response.message);
+                }else {
+        	        loadContent('adm_sistem-p_user');
+                    showBootDialog(true, BootstrapDialog.TYPE_SUCCESS, 'Information', response.message);
+                }
+            }
+        );
+    }
+</script>
