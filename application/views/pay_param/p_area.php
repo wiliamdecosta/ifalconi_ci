fba5e8b91a44af2c5bedc4c4209c07a53564d151
wiliamdecosta <wiliamdecosta@gmail.com>
2015-07-10 16:57:34 +0700

    ''

--- application/views/pay_param/p_area.php
+++ application/views/pay_param/p_area.php
@@ -79,7 +79,7 @@
         $("#area_grid_selection").bootgrid({
     	     formatters: {
                 "opt-edit" : function(col, row) {
-                    return '<a href="#" onclick="area_show_form_edit(\''+ row.p_area_id +'\')" class="green"><i class="ace-icon fa fa-pencil bigger-130"></i></a> &nbsp; <a href="#" onclick="area_delete_records(\''+ row.p_area_id +'\')" class="red"><i class="ace-icon glyphicon glyphicon-trash bigger-130"></i></a>';
+                    return '<a href="#" title="Edit" onclick="area_show_form_edit(\''+ row.p_area_id +'\')" class="green"><i class="ace-icon fa fa-pencil bigger-130"></i></a> &nbsp; <a href="#" title="Delete" onclick="area_delete_records(\''+ row.p_area_id +'\')" class="red"><i class="ace-icon glyphicon glyphicon-trash bigger-130"></i></a>';
                 }
              },
     	     rowCount:[10,25,50,100,-1],
