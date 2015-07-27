af335f91a75e6b12f883fd96ba3094949e709318
wiliamdecosta <wiliamdecosta@gmail.com>
2015-07-27 09:40:36 +0700

    ''

--- application/helpers/utilities_helper.php
+++ application/helpers/utilities_helper.php
@@ -17,4 +17,12 @@ function isValidEmail($email){
     return filter_var($email, FILTER_VALIDATE_EMAIL) && preg_match('/@.+\./', $email);
 }
 
+function html_spaces($number=1) {
+    $result = "";
+    for($i = 1; $i <= $number; $i++) {
+        $result .= "&nbsp;";
+    }
+    return $result;
+}
+
 ?>
