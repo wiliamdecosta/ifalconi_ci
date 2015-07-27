012b15d0063f51027c263b403a78d9f61e88c06d
wiliamdecosta <wiliamdecosta@gmail.com>
2015-07-27 09:40:31 +0700

    ''

--- application/controllers/panel.php
+++ application/controllers/panel.php
@@ -14,12 +14,36 @@ class Panel extends CI_Controller {
 	
 	function load_content($id) {
 	    try {
+	        $file_exist = true;
 	        check_login(true);
 	        $file = explode("-", $id);
+	        $url_file = "";
 	        if(count($file) > 1) {
-	            $this->load->view($file[0].'/'.$file[1]);
+	            if(strtolower(substr($file[1],-4)) != ".php")
+	                $file[1] .= ".php";
+	            
+	            if(file_exists(APPPATH."views/".$file[0].'/'.$file[1])) {
+	                $this->load->view($file[0].'/'.$file[1]);
+	            }else {
+	                $file_exist = false;   
+	            }
+	            
+	            $url_file = APPPATH."views/".$file[0].'/'.$file[1];
 	        }else {
-	            $this->load->view($id);
+	            if(strtolower(substr($id,-4)) != ".php")
+	                $id .= ".php";
+	                
+	            if(file_exists(APPPATH."views/".$id)) {
+	                $this->load->view($id);
+	            }else {
+	                $file_exist = false;   
+	            }
+	            
+	            $url_file = APPPATH."views/".$id;
+	        }
+	        
+	        if(!$file_exist) {
+	            $this->load->view("error_404.php");        
 	        }
 	        	        
 	    }catch(Exception $e) {
@@ -34,6 +58,7 @@ class Panel extends CI_Controller {
 	        exit;
 	    }
 	}
+	
 }
 
 /* End of file pages.php */
