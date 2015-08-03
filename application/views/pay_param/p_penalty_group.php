2f3ca66f8e763d56f88f80efbf7ef50cf4ccd003
wiliamdecosta <wiliamdecosta@gmail.com>
2015-08-03 15:29:25 +0700

    ''

--- application/views/pay_param/p_penalty_group.php
+++ application/views/pay_param/p_penalty_group.php
@@ -39,7 +39,7 @@
                   <tr>
                     <th data-identifier="true" data-visible = "false" data-header-align="center" data-align="center" data-column-id="p_penalty_group_id"> ID Stamp Group</th>
                      <th data-header-align="center" data-align="center" data-formatter="opt-edit" data-sortable="false" data-width="100">Options</th>
-                     <th data-column-id="code" data-width="200">Stamp Group Code</th>
+                     <th data-column-id="code" data-width="200">Penalty Group Code</th>
                      <th data-column-id="description"> Description </th>
                   </tr>
                 </thead>
