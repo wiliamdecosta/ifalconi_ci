9c63d6cc79d4d224072552073861f7467e281466
wiliamdecosta <wiliamdecosta@gmail.com>
2015-08-05 16:53:52 +0700

    ''

--- application/views/pay_process/check_payment_login.php
+++ application/views/pay_process/check_payment_login.php
@@ -1,22 +1,22 @@
-<?php 
+<?php
 function check_payment_login($url_redirect) {
-    
+
     /* ----------------------- check login first ------------------------ */
-    if( getVarClean("p_user_loket_id","str","") == "" or 
+    if( getVarClean("p_user_loket_id","str","") == "" or
             getVarClean("user_name","str","") == "" ) :
         echo '<script>
                 loadContentWithParams("pay_process-payment_login.php",{
                     url_redirect : "'.$url_redirect.'"
                 });
             </script>';
-        
+
     else :
-        
+
          /* re-check login */
         $ci = & get_instance();
     	$ci->load->model('pay_param/p_user_loket');
     	$table = $ci->p_user_loket;
-    	
+
     	$p_user_loket_id = $table->revalid_login( getVarClean("p_user_loket_id","int",0), getVarClean("user_name","str",""));
     	if(empty($p_user_loket_id)) :
     	    echo '<script>
@@ -24,7 +24,7 @@ function check_payment_login($url_redirect) {
                     url_redirect : "'.$url_redirect.'"
                 });
             </script>';
-    	    
+
     	endif;
     endif;
     /* ----------------------- end check login ------------------------ */
