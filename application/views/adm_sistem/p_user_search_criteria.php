82448c8c6fefbb09c8f2e23dd1eeb78343afb777
wiliamdecosta <wiliamdecosta@gmail.com>
2015-09-17 13:39:08 +0700

    ''

--- application/views/adm_sistem/p_user_search_criteria.php
+++ application/views/adm_sistem/p_user_search_criteria.php
@@ -11,7 +11,7 @@
 
 <script>
     jQuery(function($) {
-        $( ".actionBar" ).append(".search-criteria");
+        $(".actionBar").append(".search-criteria");
         $(".search-criteria").show();
     });
 </script>
